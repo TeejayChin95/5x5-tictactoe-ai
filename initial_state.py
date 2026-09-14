1e0942e7e37c88bef089da2fc68e8c78f74f79ca
Teejay Chin <teejaychin@gmail.com>
2026-09-14 17:23:34 -0400

    change k-in-a-row back to 5

--- initial_state.py
+++ initial_state.py
@@ -40,7 +40,7 @@ Board = Dict[Coordinate, str]
 State = Tuple[Board, str]
 
 BOARD_SIZE = 5
-K = 4  # how many in a row wins. team decided on 4 over 5 for more decisive games
+K = 5  # how many in a row wins. team decided on 4 over 5 for more decisive games
 PLAYERS = ('X', 'O')
 
 # 4 line directions to check from a filled cell.
